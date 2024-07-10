6f6e336715530ef1c42ce0c33a0c0ee2f395c710
Sacramento-20 <sacramento.15@hotmail.com>
2024-07-10 18:58:24 -0300

    Commit 8 - Classe Repositorio

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
 const { readFileSync } = require('fs');
-
-
-class Repositorio {
-  constructor() {
-    this.pecas = JSON.parse(readFileSync('./pecas.json'));
-  }
-
-  getPeca(apre) {
-    return this.pecas[apre.id];
-  }
-}
+var Repositorio = require("./repositorio.js");
 
 class ServicoCalculoFatura {
   
